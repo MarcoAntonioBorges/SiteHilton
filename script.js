bcf58f86fbf9607c37f18736736c7cdfb36b43e0
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2020-04-18 18:37:03 -0300

    Change background main if cel width

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-$("nav ul li a").click(function() {
-    $("nav ul li a").removeClass("ativado");
-    $(this).toggleClass("ativado");
+$(document).ready(function() {
+
+    $("nav ul li a").click(function() {
+        $("nav ul li a").removeClass("ativado");
+        $(this).toggleClass("ativado");
+    });
+
+    formatarVideoPrincipal();
+
+
+    /*
+        MODO DE PEGAR O ANO CORRENTE E INSERIR NO ELEMENTO COM O JQUERY
+        BIBLIOTECA JQUERY
+    */
+    $("#date-now").text(
+        new Date().getFullYear()
+    );
+
+
+    /* MODO DE PEGAR O ANO CORRENTE E INSERIR NO ELEMENTO HTML COM JAVASCRIPT PADRÃO
+        JAVASCRIPT DEFAULT
+    
+    var dateNowElement = document.getElementById("date-now");
+    dateNowElement.innerText = new Date().getFullYear;
+    
+    */
+
 });
 
-/*
-    MODO DE PEGAR O ANO CORRENTE E INSERIR NO ELEMENTO COM O JQUERY
-    BIBLIOTECA JQUERY
-*/
-$("#date-now").text(
-    new Date().getFullYear()
-);
-
-
-/* MODO DE PEGAR O ANO CORRENTE E INSERIR NO ELEMENTO HTML COM JAVASCRIPT PADRÃO
-    JAVASCRIPT DEFAULT
-
-var dateNowElement = document.getElementById("date-now");
-dateNowElement.innerText = new Date().getFullYear;
-
-*/
+function formatarVideoPrincipal() {
+    var tam = $(window).width();
+    var elementMain = $("#pagina-inicial");
+    var firstChild = elementMain.children().get(0);
+    var secondChild = elementMain.children().get(2);
+    if (tam <= 480) {
+        firstChild.remove();
+        secondChild.remove();
+    } else {
+        let video = elementMain.children().get(0);
+        video.remove();
+        elementMain.append(firstChild);
+        elementMain.append(video);
+        elementMain.append(secondChild);
+    }
+}
