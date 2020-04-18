80709a549a315d714bac963221aa1c946a946e4c
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2020-04-18 17:40:40 -0300

    Alteracoes das imagens,  estrutura definida
    Co-authored-by: nicolasosmundo@hotmail.com <nicolasosmundo@hotmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-$("nav ul li a").click(function() {  
+$("nav ul li a").click(function() {
     $("nav ul li a").removeClass("ativado");
-    $(this).toggleClass("ativado");  
-}); 
+    $(this).toggleClass("ativado");
+});
+
+/*
+    MODO DE PEGAR O ANO CORRENTE E INSERIR NO ELEMENTO COM O JQUERY
+    BIBLIOTECA JQUERY
+*/
+$("#date-now").text(
+    new Date().getFullYear()
+);
+
+
+/* MODO DE PEGAR O ANO CORRENTE E INSERIR NO ELEMENTO HTML COM JAVASCRIPT PADRÃO
+    JAVASCRIPT DEFAULT
+
+var dateNowElement = document.getElementById("date-now");
+dateNowElement.innerText = new Date().getFullYear;
+
+*/
