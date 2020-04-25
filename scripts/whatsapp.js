07d1aedf5d085d93ae8b1f91d34034339def3aca
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2020-04-25 16:06:05 -0300

    Config API WhatsApp
    Co-authored-by: nicolasosmundo@hotmail.com <nicolasosmundo@hotmail.com>

---
+++ scripts/whatsapp.js
@@ -0,0 +1,32 @@
+var messageModel = {
+    nome: '',
+    sobrenome: '',
+    email: '',
+    celular: '',
+    mensagem: ''
+}
+
+function montarMensagem(messageModel) {
+    return `Olá, meu nome é *${messageModel.nome} ${messageModel.sobrenome}*, \n*Descrição da Mensagem:* ${messageModel.mensagem} \n\n` +
+        `*Meios de contatos:* \n` +
+        `📱 *Celular:* ${messageModel.celular} \n` +
+        `📩 *Email:* ${messageModel.email}`;
+
+
+}
+
+function preencherMessageModel(formulario) {
+    messageModel.nome = formulario.fname.value;
+    messageModel.sobrenome = formulario.lname.value;
+    messageModel.email = formulario.email.value;
+    messageModel.celular = formulario.phone.value;
+    messageModel.mensagem = formulario.message.value;
+}
+
+$("#form-contato").submit(function(e) {
+    e.preventDefault();
+    preencherMessageModel(e.target);
+
+    var textoFormatado = window.encodeURIComponent(montarMensagem(messageModel));
+    window.open(`https://web.whatsapp.com/send?phone=++5511953172023&text=${textoFormatado}`);
+});
